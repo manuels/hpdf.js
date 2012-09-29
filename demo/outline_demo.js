3325fe86ff601e362c644141446539202905e994
manuels <manuel.schoelling@gmx.de>
2012-09-29 20:04:29 +0200

    Added outline demo

---
+++ demo/outline_demo.js
@@ -0,0 +1,65 @@
+function print_page(page, page_num) {
+  page.setWidth(800);
+  page.setHeight(800);
+
+  page.beginText();
+  page.moveTextPos(30, 740);
+  page.showText(page_num.toString());
+  page.endText();
+}
+
+var pdf = new HPDF();
+
+/* create default-font */
+var font = pdf.font("Helvetica");
+
+/* Set page mode to use outlines. */
+pdf.setPageMode('OUTLINE');
+
+var page = [];
+/* Add 3 pages to the document. */
+page[0] = pdf.addPage();
+page[0].setFontAndSize(font, 30);
+print_page(page[0], 1);
+
+page[1] = pdf.addPage();
+page[1].setFontAndSize(font, 30);
+print_page(page[1], 1);
+
+page[2] = pdf.addPage();
+page[2].setFontAndSize(font, 30);
+print_page(page[2], 1);
+
+/* create outline root. */
+var root = pdf.createOutline(undefined, "OutlineRoot");
+root.setOpened(true);
+
+var outline = []
+outline[0] = pdf.createOutline(root, "page1");
+outline[1] = pdf.createOutline(root, "page2");
+
+/* create outline with test which is ISO8859-2 encoding */
+outline[2] = pdf.createOutline(root, "ISO8859-2 text ÓÔÕÖ×ØÙ",
+                pdf.encoder("ISO8859-2"));
+
+/* create destination objects on each pages
+ * and link it to outline items.
+ */
+var dst = page[0].createDestination();
+dst.setXYZ(0, page[0].height(), 1);
+outline[0].setDestination(dst);
+//  HPDF_Catalog_SetOpenAction(dst);
+
+dst = page[1].createDestination();
+dst.setXYZ(0, page[1].height(), 1);
+outline[1].setDestination(dst);
+
+dst = page[2].createDestination();
+dst.setXYZ(0, page[2].height(), 1);
+outline[2].setDestination(dst);
+
+
+window.addFile( pdf.toDataUri() )
+
+pdf.destroy()
+
